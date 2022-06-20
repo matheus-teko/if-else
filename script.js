c0359762f07fbf6100972c140622d918695bce96
matheus-teko <matheus.work94@gmail.com>
2022-06-20 20:06:49 -0300

    if-else

---
+++ script.js
@@ -0,0 +1,21 @@
+let idade = 18;
+if(idade >= 18) {
+    console.log ("Você é maior de idade");
+} 
+else {
+    console.log ("Você é menor de idade"); 
+}
+let condicaoHumana = true;
+if (idade >= 18 && condicaoHumana === true) {
+  console.log("É maior de 18 anos");
+}
+else {
+    console.log ("é maior de 18 anos!")
+}
+let meuMes = "Dezembro";
+if (meuMes === "Dezembro" || "dezembro" || "Janeiro" || "janeiro") {
+  console.log("O meu mês é dezembro");
+}
+else {
+    console.log ("Não é janeiro!")
+}
